d63cf3a87cb33ec413e13b6d55911cb0c103ebb8
Adisa Stewart <adisa.stewart1988@gmail.com>
2016-03-26 12:54:01 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+var attackArray = [10,15,20,10,15,10,10,10]
+
+// my basic attack function
+function baseAttack() {
+	// access and decrement health
+	var health = document.getElementById('health2')
+	health.innerHTML-=attackArray[Math.floor(Math.random()*attackArray.length)]
+
+	// calls youWin when health is out
+	if(health.innerHTML<=0){
+	youWin()
+	return
+	}
+
+	// calls counter attack function in 2s
+	setTimeout(counterAtt, 1000)
+	
+}
+
+// antagonist attack
+function counterAtt() {
+	// access and decrement health
+	var health = document.getElementById('health1')
+	health.innerHTML-=attackArray[Math.floor(Math.random()*attackArray.length)]
+
+	// calls youLose when health is out
+	if(health.innerHTML<=0){
+	youLose()
+	}
+}
+
+
+// ends game when player1 wins
+function youWin() {
+	// change title
+	var title = document.getElementById('title')
+	title.innerHTML="You Win!"
+
+	// hide opponent health
+	var health = document.getElementById('health2')
+	health.style.display='none'
+
+}
+
+// ends game when player2 wins
+function youLose() {
+	// change title
+	var title = document.getElementById('title')
+	title.innerHTML="You Lose!"
+
+	// hide opponent health
+	var health = document.getElementById('health1')
+	health.style.display='none'
+
+}
+ 
+
+
+
+
+
